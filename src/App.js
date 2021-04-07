6b957c0eb4f4ef94043140d1d08bb8bfd74f733a
ShubhamSj07 <shubhamsj077@gmail.com>
2021-04-07 14:09:16 +0530

    Project Initalized with React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>ITESA WEBSITE</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
