dfa68e8e39ac07c06d3715d5ca2eadee50bc7adb
ShubhamSj07 <shubhamsj077@gmail.com>
2021-04-11 19:48:35 +0530

    Removed Parallax Effect

--- src/components/ImageSection/index.js
+++ src/components/ImageSection/index.js
@@ -1,26 +1,15 @@
 import React from "react";
-import { Parallax } from "react-parallax";
+// TODO: import { Parallax } from "react-parallax";
 
 import img from "./assets/College-Photo.jpg";
 import "./style.css";
 
-import Title from "./Title/Title";
-
 const ImageSection = () => {
 	return (
 		<>
-			<Title
-				collegename="D Y Patil College Of Engineering, Akurdi, Pune"
-				department="Department of Information Technology"
-			/>
 			<div className="about">
 				<div className="about-section">
-					<Parallax
-						bgImage={img}
-						className="aboutus-img"
-						strength={400}
-						id="parallax-effect"
-					></Parallax>
+					<img src={img} alt="" className="aboutus-img" />
 				</div>
 			</div>
 		</>
